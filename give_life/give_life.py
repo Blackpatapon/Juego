d165023f376182031768b161f64aadf79ca7b34d
Luis Angel Hidalgo Vazquez <135643171+Blackpatapon@users.noreply.github.com>
2023-11-25 12:54:57 -0600

    Add files via upload

--- give_life/give_life.py
+++ give_life/give_life.py
@@ -5,32 +5,39 @@ import time
 
 pygame.init()
 
+#Pantalla del juego
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Give Life')
 
+#Colores
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
-# Sonidos
+#Sonidos
 pygame.mixer.init()
 interference_sound = pygame.mixer.Sound('sound/interferencia.mp3')
 background_sound = pygame.mixer.music
 background_sound.load('sound/found_puppet.mp3')
 jumpsacare_sound = pygame.mixer.Sound('sound/FNAF2_Jumpscare_Sound.mp3')
 
+#Sonido de Interferencia
 interference_sound.play()
 
+#Mostrar interferencia
 for _ in range(90):
     screen.fill(red if random.randint(0, 1) == 0 else black)
     pygame.display.update()
 
+#Esperar 3 segundos
 time.sleep(3)
 
+#Detener sonido de interferencia
 interference_sound.stop()
 
+#Tamaño y Posicipn de Puppet
 puppet_size = 100
 puppet_image_left = pygame.image.load('image/puppet_left.png')
 puppet_image_right = pygame.image.load('image/puppet_right.png')
@@ -40,6 +47,7 @@ puppet_image = puppet_image_left
 puppet_x = screen_width // 2 - puppet_size // 2
 puppet_y = screen_height // 2 - puppet_size // 2
 
+#Tamaño y posicion de los niños
 niño_size = 50
 niño_images = [
     pygame.image.load('image/niños.png'),
@@ -48,8 +56,10 @@ niño_images = [
     pygame.image.load('image/niños.png')
 ]
 
+#Tamaño y posicion del regalo
 regalo_image = pygame.image.load('image/regalo.png')
 
+#Tamaño y posicion de los animatronicos
 animatronicos_images = [
     pygame.image.load('image/freddy_head.png'),
     pygame.image.load('image/bonie_head.png'),
@@ -57,16 +67,20 @@ animatronicos_images = [
     pygame.image.load('image/foxy_head.png')
 ]
 
+#Tamaño y posicion de golden freddy
 golden_freddy_image = pygame.image.load('image/golden_freddy.png')
 golden_freddy_original_size = 10
 golden_freddy_size = golden_freddy_original_size
 golden_freddy_position = [screen_width // 2 - golden_freddy_size // 2, screen_height // 2 - golden_freddy_size // 2]
 
+# Velocidad de crecimiento
 growth_speed = 1
 
+#Aparicion de las mascaras
 animatronicos_images = [pygame.transform.scale(img, (int(niño_size * 1.5), int(niño_size * 1))) for img in animatronicos_images]
 animatronicos_positions = [None] * 4
 
+#Posicion de los niños
 niños = []
 for i, position in enumerate([(50, 100), (screen_width - niño_size - 50, 100),
                               (50, screen_height - niño_size - 50), (screen_width - niño_size - 50, screen_height - niño_size - 50)]):
@@ -79,20 +93,26 @@ for i, position in enumerate([(50, 100), (screen_width - niño_size - 50, 100),
         'regalo_position': None
     })
 
+#Found me
 background_sound.play(-1)
 
+#Cuadro de movimiento
 cuadro_x = 50
 cuadro_y = 100
 cuadro_width = screen_width - cuadro_x * 2
 cuadro_height = screen_height - 150
 
+#Espacios del texto
 separacion_texto = 20
 
+#Tamaño del texto
 font_size = 55
 font = pygame.font.Font(None, font_size)
 
+#Velocidad de crecimiento
 growth_speed = 50
 
+#Tiempos de espera
 tiempo_espera = 1
 tiempo_espera_gf = 1
 tiempo_inicial = None
@@ -101,15 +121,23 @@ tiempo_inicial_regalo = None
 tiempo_inicial_animatronicos = None
 tiempo_inicial_gf = None
 
+#Velocidad del juego
 clock = pygame.time.Clock()
+
+#Estado del juego
 game_over = False
 play_jumpsacare_sound = True
 
+#Numero de "Give Life"
+give_life_number = 100
+
+# Bucle principal del juego
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    #Teclas para mover a Puppet
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and puppet_x > cuadro_x:
         puppet_x -= 5
@@ -122,6 +150,7 @@ while not game_over:
     if keys[pygame.K_DOWN] and puppet_y < cuadro_y + cuadro_height - puppet_size:
         puppet_y += 5
 
+    #Acercamiento de Puppet a los niños
     for niño in niños:
         if (
             puppet_x < niño['x'] + niño['size']
@@ -131,10 +160,12 @@ while not game_over:
         ):
             if niño['regalo_position'] is None:
                 niño['regalo_position'] = (niño['x'] - niño['size'], niño['y']) if niño['x'] > screen_width / 2 else (
-                niño['x'] + niño['size'], niño['y'])
+                    niño['x'] + niño['size'], niño['y'])
+                give_life_number += 100
         elif niño['regalo_position'] is not None:
             pass
 
+    #Jumpscare de golden freddy
     if tiempo_inicial_gf is not None:
         tiempo_transcurrido_gf = time.time() - tiempo_inicial_gf
 
@@ -151,11 +182,14 @@ while not game_over:
                 jumpsacare_sound.play()
                 play_jumpsacare_sound = False
 
+    #Los regalos aparecen
     todas_aparecidas_regalo = all(niño['regalo_position'] is not None for niño in niños)
 
+    #Tiempo de aparicion de los regalos
     if todas_aparecidas_regalo and tiempo_inicial_regalo is None:
         tiempo_inicial_regalo = time.time()
 
+    #Borrar regalos y aparecer mascaras
     if tiempo_inicial_regalo is not None and time.time() - tiempo_inicial_regalo > tiempo_espera:
         for niño in niños:
             niño['regalo_position'] = None
@@ -164,13 +198,16 @@ while not game_over:
         if tiempo_inicial_animatronicos is None:
             tiempo_inicial_animatronicos = time.time()
 
+    #Tiempo de aparicion de las mascaras
     if tiempo_inicial_animatronicos is not None and time.time() - tiempo_inicial_animatronicos > tiempo_espera:
         for i, niño in enumerate(niños):
             animatronicos_positions[i] = niño['regalo_position']
 
+        #Inicia jumpscare de golden freddy
         if all(position is not None for position in animatronicos_positions) and tiempo_inicial_gf is None:
             tiempo_inicial_gf = time.time()
 
+    #Crecimiento del jumpscare
     if tiempo_inicial_gf is not None:
         tiempo_transcurrido_gf = time.time() - tiempo_inicial_gf
 
@@ -183,10 +220,13 @@ while not game_over:
             screen.blit(pygame.transform.scale(golden_freddy_image, (golden_freddy_size, golden_freddy_size)),
                         golden_freddy_position)
 
+    # Limpiar pantalla
     screen.fill(black)
 
+    #Aparicion de Puppet
     screen.blit(puppet_image, (puppet_x, puppet_y))
 
+    #Niños y regalos en el cuadro
     for i, niño in enumerate(niños):
         screen.blit(niño['image'], (niño['x'], niño['y']))
         if niño['regalo_position'] is not None and cuadro_x < niño['regalo_position'][0] < cuadro_x + cuadro_width:
@@ -196,16 +236,20 @@ while not game_over:
         if animatronicos_positions[i] is not None:
             screen.blit(animatronicos_images[i], animatronicos_positions[i])
 
+    #Jumpscare con sonido de fondo
     if tiempo_inicial_gf is not None and tiempo_transcurrido_gf <= tiempo_espera_gf:
         screen.blit(pygame.transform.scale(golden_freddy_image, (golden_freddy_size, golden_freddy_size)),
                     golden_freddy_position)
 
+    #Borde del cuadro
     pygame.draw.rect(screen, white, (cuadro_x, cuadro_y, cuadro_width, cuadro_height), 2)
 
+    #Desaparecer el jumpscare
     if tiempo_inicial_gf is not None and tiempo_transcurrido_gf > tiempo_espera_gf:
         if tiempo_inicial is None:
             tiempo_inicial = time.time()
 
+    #Terminar el juego
     if tiempo_inicial is not None and time.time() - tiempo_inicial > 1:
         interference_sound.play()
 
@@ -219,17 +263,22 @@ while not game_over:
 
         game_over = True
 
-    text = font.render("0100 GIVE LIFE", True, white)
+    #Texto superior
+    text = font.render(f"{give_life_number:04d} GIVE LIFE", True, white)
     text_rect = text.get_rect(center=(screen_width // 2, cuadro_y - separacion_texto - text.get_height() // 2))
     screen.blit(text, text_rect)
 
+    #Actualizar
     pygame.display.update()
 
+    #Velocidad del juego
     clock.tick(30)
 
+    #Reproducir musica al terminar
     if not background_sound.get_busy():
         background_sound.play()
 
+#Cerrar
 background_sound.stop()
 pygame.quit()
 sys.exit()
